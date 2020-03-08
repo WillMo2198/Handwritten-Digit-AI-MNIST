d6cc2eee191ff05a3749175c046ca341cbf61e89
Will Morrissey <39009053+WillMo2198@users.noreply.github.com>
2020-03-08 12:30:08 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from tqdm import tqdm
 from PIL import Image
 import numpy as np
 import os
+import sys
 
-np.set_printoptions(suppress=True)
+np.set_printoptions(suppress=True, threshold=sys.maxsize)
 
 
 class ANN:
     def __init__(self):
         self.y = np.empty((10,))
-        self.weights0 = np.random.uniform(low=-1.0, high=1.0, size=(28, 64))
-        self.weights1 = np.random.uniform(low=-1.0, high=1.0, size=(64, 10))
+        self.weights1 = np.random.uniform(low=-1., high=1., size=(28, 64))
+        self.weights2 = np.random.uniform(low=-1., high=1., size=(64, 10))
+        self.bias1 = np.random.random((1,))
+        self.bias2 = np.random.random((1,))
+        self.learning_rate = .01
         self.list = []
         for x in range(28):
             self.list.append([])
 
     @staticmethod
-    def nonlin(x, deriv=False):
+    def s(x, deriv=False):
         if deriv:
             return x * (1 - x)
         else:
@@ -25,40 +29,44 @@ class ANN:
 
     def train(self, var):
         if var == 0:
-            self.y = np.array([1., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
+            self.y = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         elif var == 1:
-            self.y = np.array([0., 1., 0., 0., 0., 0., 0., 0., 0., 0.])
+            self.y = np.array([0, 1, 0, 0, 0, 0, 0, 0, 0, 0])
         elif var == 2:
-            self.y = np.array([0., 0., 1., 0., 0., 0., 0., 0., 0., 0.])
+            self.y = np.array([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
         elif var == 3:
-            self.y = np.array([0., 0., 0., 1., 0., 0., 0., 0., 0., 0.])
+            self.y = np.array([0, 0, 0, 1, 0, 0, 0, 0, 0, 0])
         elif var == 4:
-            self.y = np.array([0., 0., 0., 0., 1., 0., 0., 0., 0., 0.])
+            self.y = np.array([0, 0, 0, 0, 1, 0, 0, 0, 0, 0])
         elif var == 5:
-            self.y = np.array([0., 0., 0., 0., 0., 1., 0., 0., 0., 0.])
+            self.y = np.array([0, 0, 0, 0, 0, 1, 0, 0, 0, 0])
         elif var == 6:
-            self.y = np.array([0., 0., 0., 0., 0., 0., 1., 0., 0., 0.])
+            self.y = np.array([0, 0, 0, 0, 0, 0, 1, 0, 0, 0])
         elif var == 7:
-            self.y = np.array([0., 0., 0., 0., 0., 0., 0., 1., 0., 0.])
+            self.y = np.array([0, 0, 0, 0, 0, 0, 0, 1, 0, 0])
         elif var == 8:
-            self.y = np.array([0., 0., 0., 0., 0., 0., 0., 0., 1., 0.])
+            self.y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 1, 0])
         elif var == 9:
-            self.y = np.array([0., 0., 0., 0., 0., 0., 0., 0., 0., 1.])
+            self.y = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 1])
         for x in range(1000):
             inputs = np.asarray(self.list)
-            hidden = self.nonlin(np.dot(inputs, self.weights0))
-            outputs = self.nonlin(np.dot(hidden, self.weights1))
-            outputs_error = self.y - outputs
-            outputs_delta = outputs_error * self.nonlin(outputs, deriv=True)
-            hidden_error = outputs_delta.dot(self.weights1.T)
-            hidden_delta = hidden_error * self.nonlin(hidden, deriv=True)
-            self.weights1 += hidden.T.dot(outputs_delta)
-            self.weights0 += inputs.T.dot(hidden_delta)
+            hidden = self.s(np.dot(inputs, self.weights1) + self.bias1)
+            output = self.s(np.dot(hidden, self.weights2) + self.bias2)
+            delta_output = output - self.y
+            delta_weights2 = (1 / 100) * np.dot(hidden.T, delta_output)
+            delta_bias2 = (1 / 100) * np.sum(delta_output, axis=1, keepdims=True)
+            delta_hidden = np.multiply(np.dot(delta_output, self.weights2.T), 1 - np.power(2, hidden))
+            delta_weights1 = (1 / 100) * np.dot(inputs.T, delta_hidden)
+            delta_bias1 = (1 / 100) * np.sum(delta_hidden, axis=1, keepdims=True)
+            self.weights1 = self.weights1 - self.learning_rate * delta_weights1
+            self.bias1 = self.bias1 - self.learning_rate * delta_bias1
+            self.weights2 = self.weights2 - self.learning_rate * delta_weights2
+            self.bias2 = self.bias2 - self.learning_rate * delta_bias2
 
-    def test(self):
-        inputs = self.list
-        hidden = self.nonlin(np.dot(inputs, np.load('/weights1.npy')))
-        outputs = self.nonlin(np.dot(hidden, np.load('/weights2.npy')))
+    def test(self, image):
+        inputs = image
+        hidden = self.s(np.dot(inputs, self.weights1))
+        outputs = self.s(np.dot(hidden, self.weights2))
         print(outputs)
 
 
@@ -83,16 +91,16 @@ img6 = os.listdir(dir6)
 img7 = os.listdir(dir7)
 img8 = os.listdir(dir8)
 img9 = os.listdir(dir9)
-max0 = len(img0)
-max1 = len(img1)
-max2 = len(img2)
-max3 = len(img3)
-max4 = len(img4)
-max5 = len(img5)
-max6 = len(img6)
-max7 = len(img7)
-max8 = len(img8)
-max9 = len(img8)
+max0 = int(len(img0)/10)
+max1 = int(len(img1)/10)
+max2 = int(len(img2)/10)
+max3 = int(len(img3)/10)
+max4 = int(len(img4)/10)
+max5 = int(len(img5)/10)
+max6 = int(len(img6)/10)
+max7 = int(len(img7)/10)
+max8 = int(len(img8)/10)
+max9 = int(len(img9)/10)
 f0 = 0
 f1 = 0
 f2 = 0
@@ -195,10 +203,6 @@ for i in tqdm(range(max0+max1+max2+max3+max4+max5+max6+max7+max8+max9)):
     ann.train(rand1)
     ann.list = [[] for x in range(28)]
 
-os.system('chmod -R 777')
-np.save('/weights1.npy', ann.weights0)
-np.save('/weights2.npy', ann.weights1)
-
 while True:
     file = input('Enter file path: ')
     img = Image.open(file).convert('L')
@@ -209,9 +213,11 @@ while True:
     else:
         img_data = (img_data - np.min(img_data)) / np.ptp(img_data)
     counter = 0
-    for y in range(len(ann.list)):
+    data = []
+    for y in range(28):
+        data.append([])
+    for y in range(len(data)):
         for a in range(28):
-            ann.list[y].append(img_data[counter])
+            data[y].append(img_data[counter])
             counter += 1
-    ann.test()
-    ann.list = [[] for x in range(28)]
+    ann.test(data)
